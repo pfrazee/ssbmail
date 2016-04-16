c9f9b1add4553152904d09e509889be49bf4a37c
Paul Frazee <pfrazee@gmail.com>
2016-04-16 13:43:17 -0500

    add contacts view to profile

--- ui/views/profile.jsx
+++ ui/views/profile.jsx
@@ -5,10 +5,12 @@ import { UserPic, UserLinks } from 'patchkit-links'
 import mlib from 'ssb-msgs'
 import TopNav from '../com/topnav'
 import LeftNav from '../com/leftnav'
-import { SyncStatus } from 'mx-contact-view/oneline'
 import DropdownSelectorBtn from 'patchkit-dropdown-selector'
 import MsgList from 'mx-msg-list'
-import Oneline from 'mx-msg-view/oneline'
+import MsgOneline from 'mx-msg-view/oneline'
+import ContactList from 'mx-contact-list'
+import ContactOneline from 'mx-contact-view/oneline'
+import { SyncStatus } from 'mx-contact-view/oneline'
 import u from 'patchkit-util'
 import social from 'patchkit-util/social'
 import app from '../lib/app'
@@ -24,7 +26,7 @@ class Toolbar extends React.Component {
     return <div className="toolbar">
       <div className="toolbar-inner">
         <a className="btn toolbar-btn" href="#/contacts"><i className="fa fa-angle-left"/> Contacts</a>
-        <DropdownSelectorBtn className="btn toolbar-btn" items={VIEW_OPTS} initValue="messages" label="View" />
+        <DropdownSelectorBtn className="btn toolbar-btn" items={VIEW_OPTS} initValue={this.props.currentView} label="View" onSelect={this.props.onSelectView} />
         <div className="toolbar-divider"/>
         <a className="btn toolbar-btn" href="#">Get Contact Info</a>
         <a className="btn toolbar-btn" href="#">Verify</a>
@@ -46,18 +48,49 @@ function findLink (links, id) {
 }
 
 export default class Profile extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { view: 'messages' }
+  }
+
+  onSelectView(view) {
+    this.setState({ view: view })
+  }
+
   render() {
     const id = this.props.params && this.props.params.id
     const name = u.getName(app.users, id)
 
-    const filter = msg => !!findLink(mlib.links(msg.value.content.recps), id)
+    const msgFilter = msg => !!findLink(mlib.links(msg.value.content.recps), id)
+    const contactFilter = id2 => social.follows(app.users, id, id2)
+    const sortByName = (a, b) => u.getName(app.users, a).localeCompare(u.getName(app.users, b))
+
+    var view
+    if (this.state.view == 'messages') {
+      view = <MsgList
+        threads
+        ListItem={MsgOneline}
+        live={{ gt: [Date.now(), null] }}
+        emptyMsg={<div>No messages from {name}.</div>}
+        source={app.ssb.patchwork.createInboxStream}
+        filter={msgFilter}
+        cursor={cursor} />
+    } else if (this.state.view == 'contacts') {
+      view = <VerticalFilledContainer>
+        <ContactList ListItem={ContactOneline} filter={contactFilter} sort={sortByName} />
+      </VerticalFilledContainer>
+    } else {
+      view = <VerticalFilledContainer>
+        todo
+      </VerticalFilledContainer>
+    }
 
     return <div id="profile" key={'profile:'+id}>
       <TopNav />
       <div className="flex">
         <LeftNav location={this.props.location} />
         <div className="flex-fill">
-          <Toolbar />
+          <Toolbar currentView={this.state.view} onSelectView={this.onSelectView.bind(this)} />
           <div className="profile-header">
             <UserPic id={id} />
             <div className="info">
@@ -70,14 +103,7 @@ export default class Profile extends React.Component {
               <div>Verified by: none.</div>
             </div>
           </div>
-          <MsgList
-            threads
-            ListItem={Oneline}
-            live={{ gt: [Date.now(), null] }}
-            emptyMsg={<div>No messages from {name}.</div>}
-            source={app.ssb.patchwork.createInboxStream}
-            filter={filter}
-            cursor={cursor} />
+          {view}
         </div>
       </div>
     </div>
