b2a6cc4a77c3fcaefe9209222badcd54099f04a9
Paul Frazee <pfrazee@gmail.com>
2016-04-16 12:37:45 -0500

    add filter to contacts page

--- ui/views/contacts.jsx
+++ ui/views/contacts.jsx
@@ -5,19 +5,28 @@ import TopNav from '../com/topnav'
 import LeftNav from '../com/leftnav'
 import ContactList from 'mx-contact-list'
 import Oneline from 'mx-contact-view/oneline'
+import DropdownSelectorBtn from 'patchkit-dropdown-selector'
 import u from 'patchkit-util'
 import social from 'patchkit-util/social'
 import app from '../lib/app'
+
+const FILTER_OPTS = [
+  { label: 'Followed', value: 'followed' },
+  { label: 'Others', value: 'others' }
+]
+
+const filterFns = { 
+  followed: id => social.follows(app.users, app.user.id, id),
+  others: id => !social.follows(app.users, app.user.id, id) && id != app.user.id
+}
 
 class Toolbar extends React.Component {
   render() {
     return <div className="toolbar">
       <div className="toolbar-inner">
         <a className="btn toolbar-btn" href="#">Add New Contact</a>
-        <div className="btn-group">
-          <a className="btn toolbar-btn" href="#">Get Your Contact Info</a>
-          <a className="btn toolbar-btn btn-group-last" href="#">Open Your Profile</a>
-        </div>
+        <a className="btn toolbar-btn" href={'#/profile/'+encodeURIComponent(app.user.id)}>Open Your Profile</a>
+        <DropdownSelectorBtn className="btn toolbar-btn" items={FILTER_OPTS} initValue={this.props.currentFilter} label="Filter" onSelect={this.props.onSelectFilter} />
       </div>
     </div>
   }
@@ -26,10 +35,13 @@ class Toolbar extends React.Component {
 export default class Contacts extends React.Component {
   constructor(props) {
     super(props)
+    this.state = { filter: 'followed' }
   }
-
+  onSelectFilter(filter) {
+    this.setState({ filter: filter })
+  }
   render() {
-    const followedOnly = id => social.follows(app.users, app.user.id, id)
+    const filter = filterFns[this.state.filter]
     const sortByName = (a, b) => u.getName(app.users, a).localeCompare(u.getName(app.users, b))
 
     return <div id="contacts">
@@ -37,9 +49,9 @@ export default class Contacts extends React.Component {
       <div className="flex">
         <LeftNav location={this.props.location} />
         <div className="flex-fill">
-          <Toolbar />
+          <Toolbar currentFilter={this.state.filter} onSelectFilter={this.onSelectFilter.bind(this)} />
           <VerticalFilledContainer id="contact-list-vertical">
-            <ContactList ListItem={Oneline} filter={followedOnly} sort={sortByName} />
+            <ContactList ListItem={Oneline} filter={filter} sort={sortByName} />
           </VerticalFilledContainer>
         </div>
       </div>
